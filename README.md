9cdd6dea90ebedb747ae30b9674409b79862d598
Aksil-Abdelkhalek <aksilabdelkhalek@gmail.com>
2025-12-10 11:42:10 -0500

    Initial commit for deployment

--- README.md
+++
@@ -1,2 +0,0 @@
-# ProjectIA_mlflow_dagshub_2025
-Suivi d'expériences MLflow pour projet IA
---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import pickle
+import numpy as np
+
+app = FastAPI()
+
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+@app.get("/health")
+def health():
+    return {"status": "OK"}
+
+@app.post("/predict")
+def predict(features: list):
+    pred = model.predict([features])[0]
+    return {"prediction": float(pred)}
